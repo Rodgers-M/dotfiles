4180929ed0255db3170c6c0434d0e75abcbd2691
rogders-m <mkishagha@gmail.com>
2023-09-24 19:34:43 +0300

    add css color plugin

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -13,6 +13,9 @@ return {
 	-- Highlight yanked text
 	"machakann/vim-highlightedyank",
 
+	-- preview css colors
+	"ap/vim-css-color",
+
 	-- auto save
 	{
 		"pocco81/auto-save.nvim",
@@ -21,43 +24,16 @@ return {
 		end,
 	},
 
-	-- Detect tabstop and shiftwidth automatically
-	-- 'tpope/vim-sleuth',
-
-	-- NOTE: This is where your plugins related to LSP can be installed.
-	--  The configuration is done below. Search for lspconfig to find it below.
+	--auto close tags
 	{
-		-- LSP Configuration & Plugins
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-
-			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
-
-			-- Additional lua configuration, makes nvim stuff amazing!
-			"folke/neodev.nvim",
-		},
-	},
-
-	-- cmp
-	{
-		-- Autocompletion
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-
-			-- Adds LSP completion capabilities
-			"hrsh7th/cmp-nvim-lsp",
-
-			-- Adds a number of user-friendly snippets
-			"rafamadriz/friendly-snippets",
-		},
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
 	},
 
 	-- Useful plugin to show you pending keybinds.
