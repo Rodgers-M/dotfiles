3b61161ff9fd2246b3e45ab295f660d7d075d629
rogders-m <mkishagha@gmail.com>
2024-05-07 13:26:22 +0300

    add rust language server

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -19,6 +19,15 @@ return {
 	"mattn/emmet-vim",
 	"tpope/vim-surround",
 
+	-- rust plugin
+	"rust-lang/rust.vim",
+
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+
 	-- auto save
 	{
 		"pocco81/auto-save.nvim",
