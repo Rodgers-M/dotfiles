ba73def0349119b5e18c0eee544d7890a9a37264
rogders-m <mkishagha@gmail.com>
2024-08-31 13:40:11 +0300

    add codeium for ai code generations

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -83,6 +83,7 @@ return {
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "codeium" }, -- ai generations
 			}),
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
