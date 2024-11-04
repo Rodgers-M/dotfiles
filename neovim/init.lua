1a41d6974ea83226092a01e4d02c4e3452133c3e
rogders-m <mkishagha@gmail.com>
2024-11-04 13:21:55 +0300

    update lazyvim boostrap function

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,14 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable", -- latest stable release
+		lazyrepo,
 		lazypath,
 	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "failed to install lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\n press any key to exit" },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
+
 vim.opt.rtp:prepend(lazypath)
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
